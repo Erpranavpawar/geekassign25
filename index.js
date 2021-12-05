fcfa012617785784fb0bea9f5725542cd50dcc06
Erpranavpawar <pranavpawar7531@gmail.com>
2021-12-05 20:10:00 +0530

    assignment25

---
+++ index.js
@@ -0,0 +1,11 @@
+var data = 0;
+ document.getElementById("change_no").innerText = data
+
+ function like(){
+     data += 1;
+     document.getElementById("change_no").innerText =data }
+
+ function dislike(){
+    data -= 1;
+    document.getElementById("change_no").innerText = data
+}
